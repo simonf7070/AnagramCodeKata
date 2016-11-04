34dbeefedf75d6194603c193d59d3442d22502c1
Daniel Harrison <harrison92d+orangudan@gmail.com>
2016-11-04 09:19:25 +0000

    Added project template

---
+++ AnagramCodeKata/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramCodeKata
+{
+    class Program
+    {
+        /* 
+         * Given a file containing one word per line, print out all the combinations of words that are anagrams; 
+         * each line in the output contains all the words from the input that are anagrams of each other.
+         */
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
